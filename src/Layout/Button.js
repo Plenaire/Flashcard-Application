68733db75bda2b204dc8edcdfc210a19c4a9bf1e
Victor Gonzalez <167295210+Plenaire@users.noreply.github.com>
2024-07-29 23:50:23 +0900

    Update Button.js

--- src/Layout/Button.js
+++ src/Layout/Button.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-export const Button = ({ children, onClick, type}) => (
-    <button
-        type={ type ? { type }: "button"}
-        onClick={onClick}
-        className="btn btn-primary mb-2 mr-2 mt-2"
-    >
-        {children}
-    </button>
-);
+export const Button = ({ children, danger, ...props }) => {
+    const buttonClass = `btn ${danger ? "btn-danger" : "btn-primary"} mb-2 mr-2 mt-2 ${props.className}`;
+
+    return (
+        <button {...props} className={buttonClass}>
+            {children}
+        </button>
+    );
+};
